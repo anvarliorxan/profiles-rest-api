13ffa0c145b45c516ddcd8e8858922e336d5c720
Orxan <anvarliorxan@gmail.com>
2020-04-26 15:21:55 +0400

    Added user profile feed API

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -4,7 +4,18 @@ class UpdateOwnProfile(permissions.BasePermission):
     """Allow user to edit their own profile"""
 
     def has_object_permission(self, request, view, obj):
-        """Check user is trying to edit their own profiles"""
+        """Check the user is trying to edit their own profiles"""
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.id == request.user.id
+
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """Allow user to edit their our status"""
+
+    def has_object_permission(self, request, view, obj):
+        """Check the user is trying to update their own status"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user_profile.id == request.user.id
+         
